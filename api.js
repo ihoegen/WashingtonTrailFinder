971ca983268beee8578661e93205f1ac774b757a
Ian Hoegen <ianhoegen@gmail.com>
2016-12-03 11:00:21 -0800

    Setting up inital map layer

--- api.js
+++ api.js
@@ -3,9 +3,19 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 
 var app = express();
+
+
 app.use(bodyParser());
 
 app.use(express.static('public'));
+
+app.get('/api/', function (req, res){
+  var exec = require('child_process').exec;
+  var child = exec('java Test Hello', {cwd: './'}, function(err, stdout, stderr) {
+    console.log(stdout);
+    res.send(stdout)
+  });
+});
 
 
 app.listen(8000, function () {
