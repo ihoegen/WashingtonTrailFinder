5f088da73b74e1c00f2fe87040de350c233a66b2
Ian Hoegen <ianhoegen@gmail.com>
2016-12-03 15:26:06 -0800

    Elevation API

--- api.js
+++ api.js
@@ -10,25 +10,15 @@ app.use(bodyParser());
 
 app.use(express.static('public'));
 
-app.get('/api/test', function (req, res) {
-  var exec = require('child_process').exec;
-  var child = exec('java Test Hello', {cwd: './'}, function(err, stdout, stderr) {
-    console.log(stdout);
-    res.send(stdout)
-  });
-});
-
 app.post('/api/elevation', function (req, res){
+  var exec = require('child_process').exec;
   console.log(req.body);
   var lat = req.body.lat;
   var lng = req.body.lng;
-
-  request('https://maps.googleapis.com/maps/api/elevation/json?locations=' + lat + ',' + lng + '&key=AIzaSyCZvfnTZCqtieHQH1Vsmby3kT_sGZ5K1UI', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var result = JSON.parse(body);
+  var child = exec('java -cp ./public/src/java/ Coordinates ' + lat + " " + lng, {cwd: './'}, function(err, stdout, stderr) {
+    var result = JSON.parse(stdout);
     res.send(result.results[0].elevation + "");
-  }
-})
+  });
 });
 
 
---
+++ public/src/java/Coordinates.java
@@ -0,0 +1,19 @@
+import java.net.*;
+import java.io.*;
+
+public class Coordinates {
+
+  public static void main(String[] args) throws Exception {
+    String lat = args[0];
+    String lng = args[1];
+    String coordinateAPI = "https://maps.googleapis.com/maps/api/elevation/json?locations=" + lat + "," + lng + "&key=AIzaSyCZvfnTZCqtieHQH1Vsmby3kT_sGZ5K1UI";
+    URL url = new URL(coordinateAPI);
+    URLConnection connection = url.openConnection();
+    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    String inputLine;
+    while ((inputLine = in.readLine()) != null) {
+      System.out.println(inputLine);
+    }
+      in.close();
+  }
+}
