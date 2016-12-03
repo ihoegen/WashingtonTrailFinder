82b78fef9426708be9eac1389253ce1156313ea0
Ian Hoegen <ianhoegen@gmail.com>
2016-12-03 14:03:54 -0800

    Data commit

--- public/src/js/Map.js
+++ public/src/js/Map.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 
 function initMap() {
   var uluru = {lat: 47.582127, lng: -122.1495682};
@@ -5,8 +6,6 @@ function initMap() {
     zoom: 20,
     center: uluru
   });
-
-
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
@@ -16,11 +15,21 @@ function initMap() {
   });
 
   var overlay;
+  drawRandomLines(map);
+};
+
+function drawRandomLines(map) {
+  var data =  new Coordinate(47.582127, -122.1495682);
+  var seg = new LineSegment(data);
+  seg.Add(new Coordinate(12, 21));
+  console.log(seg.SegmentArray);
+  console.log(seg.Get(1));
+
   var line = [
-  {lat: 47.582127, lng: -122.1495682},
-  {lat: 48.291, lng: -110.821},
-  {lat: 49.142, lng: -90.431},
-  {lat: 50.467, lng: -70.027}
+  {lat: Math.random()*45, lng: Math.random()*360 - 180},
+  {lat: Math.random()*45, lng: Math.random()*360 - 180},
+  {lat: Math.random()*45, lng: Math.random()*360 - 180},
+  {lat: Math.random()*45, lng: Math.random()*360 - 180}
 ];
 var lineObj = new google.maps.Polyline({
   path: line,
@@ -29,6 +38,30 @@ var lineObj = new google.maps.Polyline({
   strokeOpacity: 1.0,
   strokeWeight: 2
 });
-
 lineObj.setMap(map);
-};
+}
+
+function Coordinate(lat, lng) {
+  this.lat = lat;
+  this.lng = lng;
+}
+
+function LineSegment(Start) {
+  this.SegmentArray = [Start];
+  this.root = Start;
+  var _this = this;
+  this.Add = function(Coordinate) {
+    _this.SegmentArray.push(Coordinate);
+  }
+  this.Get = function(index) {
+    return _this.SegmentArray[index];
+  }
+  this.GetArray = function() {
+    return SegmentArray;
+  }
+  this.AddMultiple = function(CoordArray) {
+    for (var i in CoordArray) {
+      _this.SegmentArray.push(CoordArray[i])
+    }
+  }
+}
