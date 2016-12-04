ea59e0783dac84d9f901f1dda0a2c2bf8895cd15
Myles Haynes <myleschaynes@gmail.com>
2016-12-03 16:13:05 -0800

    I'm commiting

--- public/src/js/Map.js
+++ public/src/js/Map.js
@@ -1,9 +1,47 @@
+var map;
+var poly;
+function closePolyLine() {
+  logElevation(poly.getPath());
+  console.log("Closing polyline....");
+  poly = new google.maps.Polyline({
+    strokeColor: '#000000',
+    strokeOpacity: 1.0,
+    strokeWeight: 3
+  });
+  // poly.addListener('click', closePolyLine);
+  poly.addListener('click', function () {
+    logElevation(this.getPath());
+  });
+  poly.setMap(map);
+}
+
+
+function logElevation(path) {
+  for (var i = 0; i < path.getLength(); i++) {
+    getElevation(new Coordinate(path.getAt(i).lat(), path.getAt(i).lng()), function(data) {
+      console.log(data);
+    });
+  }
+}
+// Handles click events on a map, and adds a new point to the Polyline.
+function addLatLng(event) {
+  var path = poly.getPath();
+  path.push(event.latLng);
+}
+
 function initMap() {
   var uluru = {lat: 47.582127, lng: -122.1495682};
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 20,
     center: uluru
   });
+
+  poly = new google.maps.Polyline({
+    strokeColor: '#000000',
+    strokeOpacity: 1.0,
+    strokeWeight: 3
+  });
+
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
@@ -11,33 +49,37 @@ function initMap() {
   map.addListener("bounds_changed", function(e) {
     // alert(map.getBounds());
   });
+  poly.setMap(map);
+  poly.addListener('click', closePolyLine);
+  // Add a listener for the click event
+  map.addListener('click', addLatLng);
 
   var overlay;
   drawRandomLines(map);
-};
-
-function drawRandomLines(map) {
-  var data =  new Coordinate(47.582127, -122.1495682);
-  var seg = new LineSegment(data);
-  seg.Add(new Coordinate(12, 21));
-  console.log(seg.SegmentArray);
-  console.log(seg.Get(1));
-
-  var line = [
-  {lat: Math.random()*45, lng: Math.random()*360 - 180},
-  {lat: Math.random()*45, lng: Math.random()*360 - 180},
-  {lat: Math.random()*45, lng: Math.random()*360 - 180},
-  {lat: Math.random()*45, lng: Math.random()*360 - 180}
-];
-var lineObj = new google.maps.Polyline({
-  path: line,
-  geodesic: true,
-  strokeColor: '#FF0000',
-  strokeOpacity: 1.0,
-  strokeWeight: 2
-});
-lineObj.setMap(map);
 }
+
+// function drawRandomLines(map) {
+//   var data =  new Coordinate(47.582127, -122.1495682);
+//   var seg = new LineSegment(data);
+//   seg.Add(new Coordinate(12, 21));
+//   console.log(seg.SegmentArray);
+//   console.log(seg.Get(1));
+//
+//   var line = [
+//   {lat: Math.random()*45, lng: Math.random()*360 - 180},
+//   {lat: Math.random()*45, lng: Math.random()*360 - 180},
+//   {lat: Math.random()*45, lng: Math.random()*360 - 180},
+//   {lat: Math.random()*45, lng: Math.random()*360 - 180}
+// ];
+// var lineObj = new google.maps.Polyline({
+//   path: line,
+//   geodesic: true,
+//   strokeColor: '#FF0000',
+//   strokeOpacity: 1.0,
+//   strokeWeight: 2
+// });
+// lineObj.setMap(map);
+// }
 
 function Coordinate(lat, lng) {
   this.lat = lat;
@@ -74,6 +116,3 @@ function getElevation(CoordinateObj, callback) {
   });
 
 }
-getElevation(new Coordinate(47.583822, -122.1458737), function(data) {
-  console.log(data);
-});
